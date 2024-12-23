545d9a2d8903735d02f578a640551ccf8cec41d0
PhucNguyen204 <nnphuchust@gmail.com>
2024-12-24 01:15:15 +0700

    fix hàm khởi tạo

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -4,25 +4,27 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-// note: có 1 main admin là người gọi ban đầu của hợp đồng, người này có thể add và remove những admin khác
-// những voter chỉ được add và remove bởi admin
 
+// Note: Có 1 main admin là người gọi ban đầu của hợp đồng, người này có thể add và remove những admin khác
+// Những voter chỉ được add và remove bởi admin
 
 contract WhitelistUpgradeableV2 is Initializable, OwnableUpgradeable, AccessControlUpgradeable {
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE"); // mã hóa role admin
-    bytes32 public constant VOTER_ROLE = keccak256("VOTER_ROLE"); // mã hóa role voter
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE"); // Mã hóa role admin
+    bytes32 public constant VOTER_ROLE = keccak256("VOTER_ROLE"); // Mã hóa role voter
 
     event RoleAdded(address indexed account, bytes32 role);
     event RoleRemoved(address indexed account, bytes32 role);
 
-    function initialize(address initialOwner) public initializer { //kiểm tra lại hàm này
-        __AccessControl_init(); 
-        _transferOwnership(initialOwner);
-        _grantRole(ADMIN_ROLE, initialOwner); // người đầu tiên gọi hợp đồng sẽ được cấp quyền admin
-        _setRoleAdmin(VOTER_ROLE, ADMIN_ROLE);
+    function initialize(address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
+        __AccessControl_init();
+
+        _grantRole(ADMIN_ROLE, initialOwner); // Người đầu tiên gọi hợp đồng sẽ được cấp quyền admin
+        _setRoleAdmin(VOTER_ROLE, ADMIN_ROLE); // ADMIN_ROLE quản lý VOTER_ROLE
+        _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE); // ADMIN_ROLE tự quản lý ADMIN_ROLE
     }
 
-    function addAdmin(address admin) public onlyOwner { // chỉ sở hữa hợp đồng mới có thể thêm admin
+    function addAdmin(address admin) public onlyOwner { // Chỉ sở hữu hợp đồng mới có thể thêm admin
         grantRole(ADMIN_ROLE, admin);
         emit RoleAdded(admin, ADMIN_ROLE);
     }
