26d5352a741ec435c21a041bf87e237b162e9ff1
Doan Dat <144463212+CptDat9@users.noreply.github.com>
2024-12-24 11:17:48 +0700

    Update testWhitelist.js

--- test/testWhitelist.js
+++ test/testWhitelist.js
@@ -27,16 +27,17 @@ describe("WhitelistUpgradeableV2", function () {
     });
 
     it("Non-admin ko the add voters", async function () {
+        const ADMIN_ROLE = await whitelist.ADMIN_ROLE();
+
         await whitelist.connect(owner).addAdmin(admin.address);
         await whitelist.connect(admin).addVoter(voter.address);
 
         await expect(whitelist.connect(nonAdmin).addVoter(voter.address))
-            .to.be.revertedWith(
-                `AccessControl: account ${nonAdmin.address.toLowerCase()} is missing role ${await whitelist.ADMIN_ROLE()}`
-            );
+        .to.be.revertedWithCustomError(whitelist, "AccessControlUnauthorizedAccount")
+        .withArgs(nonAdmin.address, ADMIN_ROLE);
     });
 
-    it("Owner co the remove admin", async function () {
+    it("Owner co the remove admin", async function () { 
         const ADMIN_ROLE = await whitelist.ADMIN_ROLE();
 
         await whitelist.connect(owner).addAdmin(admin.address);
@@ -69,3 +70,37 @@ describe("WhitelistUpgradeableV2", function () {
             .withArgs(admin.address, ADMIN_ROLE);
     });
 });
+/*
+const {expect} = require("chai");
+const {ethers, upgrades} = require("hardhat");
+describe("WhitelistUpgradeableV2", function() {
+    let whitelist, owner, admin, voter, other, nonAdmin;
+    beforeEach(async function(){
+        [owner, admin, voter, other, nonAdmin] = await ethers.getSigner();
+        const WhitelistFactory = await ethers.getContractFactory("WhitelistUpgradeableV2");
+        whitelist = await upgrades.deployProxy(WhitelistFactory, [owner.address], { initializer: "initialize" });
+    });
+    it("Owner có admin role ?", async function () {
+        const ADMIN_ROLE = await whitelist.ADMIN_ROLE();
+        expect(await whitelist.hasRole(ADMIN_ROLE, owner.address)).to.equal(true);
+    });
+    it("Admin co the add voter, voter có role hợp lệ.", async function () {
+        const ADMIN_ROLE = await whitelist.ADMIN_ROLE();
+        const VOTER_ROLE = await whitelist.VOTER_ROLE();
+        await whitelist.connect(owner).addAdmin(admin.address);
+        expect(await whitelist.hasRole(ADMIN_ROLE, admin.address)).to.equal(true);
+        await whitelist.connect(admin).addVoter(voter.address);
+        expect(await whitelist.hasRole(VOTER_ROLE, voter.address)).to.equal(true);
+        expect(await whitelist.isVoter(voter.address)).to.equal(true);
+    });
+    it("Non admin khong the add voter", async function () {
+        const ADMIN_ROLE = await whitelist.ADMIN_ROLE();
+        const VOTER_ROLE = await whitelist.VOTER_ROLE();
+        await whitelist.conncect(owner).addAdmin(admin.address);
+        await whitelist.conncect(admin).addVoter(voter.address);
+        await expect(whitelist.connect(nonAdmin).addVoter(voter.address)).to.be
+        .revertedWith(`AccessControl: account ${nonAdmin.address.toLowerCase()} is missing role ${await whitelist.ADMIN_ROLE()}`);
+    });
+ // Errror kieu khac
+});
+*/
